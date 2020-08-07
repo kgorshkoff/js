16ffb61a6c30fd9449b8dbc2752704f83db7b5eb
Kirill Gorshkoff <gobzter@gmail.com>
2020-08-07 22:04:36 +0300

    added simple array basket with total price

--- public/hw6/src/components/js/basket.js
+++ public/hw6/src/components/js/basket.js
@@ -1,4 +1,11 @@
+let basket = [];
+let total = 0;
+let onAddToBasket = document.querySelectorAll(".buyButton");
 
-
-let onAddToBasket = document.querySelector('.d-md-none');
-onAddToBasket.onclick = () => { console.log('clicked') };
+onAddToBasket.forEach(e => {
+    e.addEventListener('click', evt => {
+        basket.push([evt.currentTarget.dataset.name, evt.currentTarget.dataset.price]);
+        total += parseInt(evt.currentTarget.dataset.price);
+        })
+    }
+);
