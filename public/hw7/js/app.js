8b26363236955e373f86e7ebd5df6f8b0cd5d7af
Kirill Gorshkoff <gobzter@gmail.com>
2020-08-11 18:02:37 +0300

    добавил пипетку, но по какой-то причине не устанавливается по клику нужный цвет в палитру, не подскажете, в чем может быть пробелма?

--- public/hw7/js/app.js
+++ public/hw7/js/app.js
@@ -28,6 +28,9 @@ const abobe = {
     },
     _handleEvents() {
         this.canvas.addEventListener('mousemove', this._moveHandler.bind(this));
+        this.canvas.addEventListener('mousedown', this._start.bind(this));
+        document.addEventListener('mouseup', this._stop.bind(this));
+
         this.tools.addEventListener('click', this._clickHandler.bind(this));
         this.tools.addEventListener('change', this._changeHandler.bind(this));
     },
@@ -37,22 +40,75 @@ const abobe = {
         this._renderCoordinates(this.x, this.y);
     },
     _clickHandler(evt) {
-        this.editor.currentTool = evt.target.dataset.tool;
+        if (evt.target.name === 'tool') {
+            this.editor['currentTool'] = evt.target.dataset.tool;
+        }
+        if (evt.target.name === 'save') {
+            this._save();
+        }
     },
     _changeHandler(evt) {
-        switch (evt.target.dataset.tool) {
-            case "Color":
-                this.editor.currentColor = evt.target.value;
-                break
-            case "BrushSize":
-                this.editor.currentBrushSize = evt.target.value;
-                break
+        if (evt.target.name === 'tool-input') {
+            this.editor[`current${evt.target.dataset.tool}`] = evt.target.value;
         }
     },
     _renderCoordinates(x, y) {
         document.querySelector('#xCoord').innerText = x;
         document.querySelector('#yCoord').innerText = y;
     },
+    _start() {
+        this.ctx.fillStyle = this.editor.currentColor;
+        this[`_${this.editor.currentTool}`]();
+    },
+    _stop() {
+        this.canvas.onmousemove = null;
+    },
+    _pencil() {
+        let size = this.editor.currentBrushSize;
+        this.canvas.onmousemove = () => {
+            this.ctx.fillRect(this.x, this.y, size, size);
+        }
+    },
+    _fill() {
+        this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+    },
+    _eraser() {
+        this.ctx.fillStyle = '#FFFFFF';
+        let size = this.editor.currentBrushSize;
+
+        this.canvas.onmousemove = () => {
+            this.ctx.fillRect(this.x, this.y, size, size);
+        }
+    },
+    _eyeDropper() {
+        let color = this.ctx.getImageData(this.x, this.y, 1, 1).data;
+        let hex = [color[0], color[1], color[2]].map(function(x){
+            x = parseInt(x).toString(16);
+        return (x.length==1) ? "0" + x : x;
+        });
+
+        this.canvas.onclick = function () {
+            this.editor.currentColor = '#' + hex.join('');
+        }
+
+    },
+    _clear() {
+        this.ctx.fillStyle = '#FFFFFF';
+        this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+    },
+    _save() {
+        let img = new Image();
+        img.src = this.canvas.toDataURL();
+
+        let link = document.createElement('a');
+        link.setAttribute('href', img.src);
+        link.setAttribute('download', 'canvasImage');
+
+        link.click();
+    },
+    rgbToHex(arr) {
+
+    }
 
 }
 
